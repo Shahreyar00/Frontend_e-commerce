43182040bd7140ee013d20264820806d6e9f4d67
Shahreyar Arif <70688937+Shahreyar00@users.noreply.github.com>
2021-12-11 10:59:13 +0530

    first commit

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+    position: relative;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 10px;
+    ${mobile({height:"20vh"})} 
+`;
+
+const Info = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Title = styled.div`
+    color: white;
+    margin-bottom: 20px;
+`;
+
+const Button = styled.button`
+    border: none;
+    padding: 10px;
+    font-size: 20px;
+    color: white;
+    cursor: pointer;
+    font-weight: 600;
+    border-radius: 10px;
+    background: rgba(255,255,255,0.05);
+    box-shadow: 0 10px 25px rgba(0,0,0,0.2); 
+    backdrop-filter: blur(5px);
+`;
+
+const CategoryItem = ({ item }) => {
+    return (
+        <Container>
+            <Image src={item.img} />
+            <Info>
+                <Title>{item.title}</Title>
+                <Button>SHOP NOW</Button>
+            </Info>
+        </Container>
+    )
+}
+
+export default CategoryItem
+
+
+
